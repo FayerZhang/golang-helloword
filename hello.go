4a683ed131b8d576133805a5527fade806f3ecee
zhangfeifei1989 <feifei.zhang@huawei.com>
2019-09-20 14:58:47 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,15 +1,27 @@
 package main
-import(
-	"fmt"
-	"log"
-	"net/http"
+
+import (
+    "io"
+    "net/http"
+    "log"
+    "os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    version := os.Getenv("version")
+    SYS_INCREMENT := os.Getenv("SYS_INCREMENT")
+    SYS_TIMESTAMP := os.Getenv("SYS_TIMESTAMP")
+    io.WriteString(w, "hello, world!\n")
+    io.WriteString(w, "version="+ version + "\n")
+    io.WriteString(w, "SYS_INCREMENT="+ SYS_INCREMENT + "\n")
+    io.WriteString(w, "SYS_TIMESTAMP="+ SYS_TIMESTAMP + "\n")
 }
 
 func main() {
-	 http.HandleFunc("/", handler)
-	 log.Fatal(http.ListenAndServe(":8080",nil))
+    http.HandleFunc("/hello", HelloServer)
+    err := http.ListenAndServe(":12345", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
